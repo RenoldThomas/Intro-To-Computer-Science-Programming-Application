3c1c46b96cab85fa76686af57d151c93970df71e
Renold Thomas <renoldthomasp@gmail.com>
2025-01-16 10:23:51 -0500

    Complete assign02

--- renoldthomaslabs/CS0410Assign2/Calc.java
+++ renoldthomaslabs/CS0410Assign2/Calc.java
@@ -7,6 +7,28 @@ package renoldthomaslabs.CS0410Assign2;
 
 public class Calc {
     public static void main(String[] args) {
-
+        int firstNum = 29645;
+        int secondNum = 392;
+        int sum; // sum of firstNum and secondNum
+        int diff; // difference of firstNum and secondNum
+        int product; // product of firstNum and secondNum
+        double quotient; // firstNum divided by secondNum
+        double average; // average of firstNum and secondNum
+        int remainder; // remainder when firstNum is divided by secondNum
+        
+        sum = firstNum + secondNum;
+        diff = firstNum - secondNum;
+        product = firstNum * secondNum;
+        quotient = firstNum / secondNum;
+        average = (firstNum + secondNum) / 2;
+        remainder = firstNum % secondNum;
+        
+        System.out.println("Sum is " + sum);
+        System.out.println("Difference is " + diff);
+        System.out.println("Product is " + product);
+        System.out.println("Quotient is " + quotient);
+        System.out.println("Average is " + average);
+        System.out.println("Remainder is " + remainder);
+        
     }
 }
