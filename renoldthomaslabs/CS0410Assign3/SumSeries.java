304b3cfbac50abf6d0d1102e1052fb80d02cd009
Renold Thomas <renoldthomasp@gmail.com>
2025-01-23 10:13:29 -0500

    Create shell for assignment 3

---
+++ renoldthomaslabs/CS0410Assign3/SumSeries.java
@@ -0,0 +1,12 @@
+// Renold Thomas
+// SumSeries.java
+// 23 January 2025 @ 10-10:50 AM
+// Program to calculate a sum series
+
+package renoldthomaslabs.CS0410Assign3;
+
+public class SumSeries {
+    public static void main(String[] args) {
+        
+    }
+}
