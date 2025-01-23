10f2613a09ad8adf50945fb0c6259c70c5d981e6
Renold Thomas <renoldthomasp@gmail.com>
2025-01-23 15:34:05 -0500

    Complete assignment 3 to calculate sum series

--- renoldthomaslabs/CS0410Assign3/SumSeries.java
+++ renoldthomaslabs/CS0410Assign3/SumSeries.java
@@ -5,8 +5,26 @@
 
 package renoldthomaslabs.CS0410Assign3;
 
+import java.util.Scanner;
+
 public class SumSeries {
     public static void main(String[] args) {
-        
-    }
+        Scanner powerInput = new Scanner(System.in);
+        Scanner ratioInput = new Scanner(System.in);
+        int highestPower;
+        double ratio;
+        double sumSeries;
+
+        // Get inputs from user
+        System.out.println("Enter a positive integer for n (highest power): ");
+        highestPower = powerInput.nextInt();
+        System.out.println("Enter a real number for r (ratio): ");
+        ratio = ratioInput.nextDouble();
+
+        // Sum Series Calculations
+        sumSeries = ((Math.pow(ratio, highestPower + 1) - ratio) / (ratio - 1));
+
+        // Print results
+        System.out.printf("\nSum is %.2f", sumSeries);
+    }  
 }
