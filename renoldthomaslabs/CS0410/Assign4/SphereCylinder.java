25abeda79853d4479ea0930ed29c240557be44bf
Renold Thomas <renoldthomasp@gmail.com>
2025-01-30 10:50:07 -0500

    Finish and tested assignment 4 for submission

--- renoldthomaslabs/CS0410/Assign4/SphereCylinder.java
+++ renoldthomaslabs/CS0410/Assign4/SphereCylinder.java
@@ -9,6 +9,52 @@ import java.util.Scanner;
 
 public class SphereCylinder {
     public static void main(String[] args) {
-        
+        Scanner userInput = new Scanner(System.in);
+        double volume;
+        double surfaceArea;
+        double answer;
+        double radius;
+        double height;
+        int shape;
+        int calculation;
+
+        // Get inputs from user
+        System.out.print("Choose shape: Sphere (1), Cylinder (2).... ");
+        shape = userInput.nextInt();
+
+        System.out.print("Choose calculation: Volume (1), Surface Area (2).... ");
+        calculation = userInput.nextInt();
+
+        // Calculations and shape decisions
+        // Sphere
+        if (shape == 1) {
+            System.out.print("Enter radius.... ");
+            radius = userInput.nextDouble();
+            // Volume
+            if (calculation == 1) {
+                answer = (4 / (double)3) * Math.PI * Math.pow(radius, 3);
+            }
+            // Surface Area
+            else {
+                answer = 4 * Math.PI * Math.pow(radius, 2);
+            }
+        }
+        // Cylinder
+        else {
+            System.out.print("Enter radius.... ");
+            radius = userInput.nextDouble();
+            
+            System.out.print("Enter height.... ");
+            height = userInput.nextDouble();
+            // Volume
+            if (calculation == 1) {
+                answer = Math.PI * Math.pow(radius, 2) * height;
+            }
+            // Surface Area
+            else {
+                answer = (2 * Math.PI * radius * height) + (2 * Math.PI * Math.pow(radius, 2));
+            }
+        }
+        System.out.printf("Answer = %.4f", answer);
     }
 }
