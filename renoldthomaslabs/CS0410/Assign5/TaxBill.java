2e4c35a2873643659fc2042fc9a1ce6f2b53dc27
Renold Thomas <renoldthomasp@gmail.com>
2025-02-06 09:40:47 -0500

    Create template for lab 05 for CS410

---
+++ renoldthomaslabs/CS0410/Assign5/TaxBill.java
@@ -0,0 +1,14 @@
+// Renold Thomas
+// SumSeries.java
+// 06 February 2025 @ 10-10:50 AM
+// Program to calculate the volume or surface area of a sphere/closed cylinder
+
+package renoldthomaslabs.CS0410.Assign5;
+
+import java.util.Scanner;
+
+public class TaxBill {
+    public static void main(String[] args) {
+        
+    }
+}
