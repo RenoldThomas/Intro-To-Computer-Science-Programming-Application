68bc346247736094edcc601435d3b2061498641f
Renold Thomas <renoldthomasp@gmail.com>
2025-02-13 11:25:12 -0500

    Complete assign 6 for CS0410 employee ID creation

--- renoldthomaslabs/CS0410/Assign6/EmployeeID.java
+++ renoldthomaslabs/CS0410/Assign6/EmployeeID.java
@@ -15,7 +15,99 @@ public class EmployeeID {
         String birthMonth;
 
         Scanner input = new Scanner(System.in);
-        lastName = input.next();
+
+        System.out.println("Enter your last name: " );
+        lastName = input.next().toUpperCase();
+        System.out.println("Enter your department name (3 letter abbreviation): ");
+        department = input.next().toUpperCase();
+        System.out.println("Enter your birth month (3 letter abbreviation): ");
+        birthMonth = input.next().toUpperCase();
+
+        //Switch to determine the first letter of ID based on first character of last name
+        switch (lastName.substring(0, 1)) {
+            case "A":
+            case "E":
+            case "I":
+            case "O":
+            case "U":
+                employeeID = "V";
+                break;
+            default:
+                employeeID = "C";
+        }
+
+        //Department code for employee ID
+        switch(department) {
+            case "ACC":
+                employeeID += "101";
+                break;
+            case "HRS":
+                employeeID += "102";
+                break;
+            case "SAL":
+                employeeID += "103";
+                break;
+            case "FIN":
+                employeeID += "104";
+                break;
+            case "ADV":
+                employeeID += "105";
+                break;
+            case "COM":
+                employeeID += "106";
+                break;
+            default:
+                employeeID += "110";
+            
+        }
+
+        //Birth month code for employee ID
+        switch(birthMonth) {
+            case "JAN":
+            case "FEB":
+                employeeID += "01";
+                break;
+            case "MAR":
+            case "APR":
+                employeeID += "02";
+                break;
+            case "MAY":
+            case "JUN":
+                employeeID += "03";
+                break;
+            case "JUL":
+            case "AUG":
+                employeeID += "04";
+                break;
+            case "SEP":
+            case "OCT":
+                employeeID += "05";
+                break;
+            case "NOV":
+            case "DEC":
+                employeeID += "06";
+                break;
+        }
+
+        //Last 2 characters of ID based on length of last name
+        switch(lastName.length()) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+                employeeID += "0" + lastName.length();
+                break;
+            default:
+                employeeID += lastName.length();
+
+        }
+
+        System.out.println("Your employee ID is: " + employeeID);
 
     }
 }
