8943a2fe8933aa0767fb287524c43f0c49096d23
Renold Thomas <renoldthomasp@gmail.com>
2025-02-13 10:17:39 -0500

    Create template for java file and initialize vars.

---
+++ renoldthomaslabs/CS0410/Assign6/EmployeeID.java
@@ -0,0 +1,21 @@
+// Renold Thomas
+// EmployeeID.java
+// 13 February 2025 @ 10-10:50 AM
+// Program to display employee's ID given last name, department and birth month.
+
+package renoldthomaslabs.CS0410.Assign6;
+
+import java.util.Scanner;
+
+public class EmployeeID {
+    public static void main(String[] args) {
+        String employeeID;
+        String lastName;
+        String department;
+        String birthMonth;
+
+        Scanner input = new Scanner(System.in);
+        lastName = input.next();
+
+    }
+}
