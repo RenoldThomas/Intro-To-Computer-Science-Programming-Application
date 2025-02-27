19d484169bc99a9164a7f6b1bc9794296e15e0b8
Renold Thomas <renoldthomasp@gmail.com>
2025-02-27 10:07:46 -0500

    Template for InClass Assignment SumOddSquares

---
+++ renoldthomaslabs/CS0410/InClassAssign/SumOddSquares.java
@@ -0,0 +1,12 @@
+// Renold Thomas
+// SumOddSquares.java
+// 27 February 2025 @ 10-10:50 AM
+// Program to input an odd positive integer, n, and then calculate and print the sum 12 + 32 + 52 + 72 + ....... + n2
+
+package renoldthomaslabs.CS0410.InClassAssign;
+
+public class SumOddSquares {
+    public static void main(String[] args) {
+
+    }
+}
