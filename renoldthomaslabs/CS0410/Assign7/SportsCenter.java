1792995439159a360d0378cabdc0a8a36565f283
Renold Thomas <renoldthomasp@gmail.com>
2025-02-27 11:42:02 -0500

    Create template for Assign 7 SportsCenter.
    Created constants and initialized variables that I think are needed to
    get started on this assignment. Will update if needed.

---
+++ renoldthomaslabs/CS0410/Assign7/SportsCenter.java
@@ -0,0 +1,43 @@
+// Renold Thomas
+// SumOddSquares.java
+// 27 February 2025 @ 10-10:50 AM
+// Program to prints the cost of the activity, the parking cost, the discount and the final price based on certain inputs.
+
+package renoldthomaslabs.CS0410.Assign7;
+
+import java.util.Scanner;
+
+public class SportsCenter { 
+    public static void main(String[] args) {
+        final double NM_PEAK = 10.00;
+        final double NM_OFF_PEAK = 9.00;
+        final double NM_PARKING = 5.00;
+    
+        final double M_PEAK = 7.50;
+        final double M_OFF_PEAK = 6.50;
+        final double M_PARKING = 3.50;
+
+        final double PM_PEAK = 5.00;
+        final double PM_OFF_PEAK = 4.00;
+        final double PM_PARKING = 2.00;
+
+        final double EM_PEAK = 3.50;
+        final double EM_OFF_PEAK = 3.00;
+        final double EM_PARKING = 0.00;
+
+        final double CONCESSION_DISCOUNT = 0.90;
+
+        Scanner input = new Scanner(System.in);
+        String membershipType;
+        String peakTime;
+        String concessions;
+        double hoursSpent;
+
+        double activityCost;
+        double parkingCost;
+        double discountPrice;
+        double totalCost;
+
+    }
+
+}
