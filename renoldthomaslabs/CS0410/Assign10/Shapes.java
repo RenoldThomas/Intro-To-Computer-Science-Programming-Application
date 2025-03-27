4a57ca9a18817396d9155ae8ee3ffe86aad6df3d
Renold Thomas <renoldthomasp@gmail.com>
2025-03-27 09:33:18 -0400

    Initial template for assignment 10 CS0410

---
+++ renoldthomaslabs/CS0410/Assign10/Shapes.java
@@ -0,0 +1,13 @@
+// Renold Thomas
+// Shapes.java
+// 27 March 2025 @ 10-10:50 AM
+// Program to calculate desired properties of a a desired shape.
+
+package renoldthomaslabs.CS0410.Assign10;
+import java.util.Scanner;
+
+public class Shape {
+    public static void main(String[] args) {
+        
+    }
+}
