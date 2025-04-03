6405858e0de04fb6d95ddbd4fd4a0ee757285381
Renold Thomas <renoldthomasp@gmail.com>
2025-04-03 10:24:30 -0400

    Create desired calculations for the given inputs

--- renoldthomaslabs/CS0410/Assign11/StatsArray.java
+++ renoldthomaslabs/CS0410/Assign11/StatsArray.java
@@ -18,6 +18,45 @@ public class StatsArray {
         for (int i = 0; i < size; i++) {
             numbers[i] = input.nextInt();
         }
-        
+
+        // Find highest number
+        int highestNumber = numbers[0];
+        for (int num : numbers) {
+            if (num > highestNumber) {
+                highestNumber = num;
+            }
+        }
+        System.out.println("The highest number is: " + highestNumber);
+
+        // Calculate and print mean
+        double mean = 0;
+        for (int num : numbers) {
+            mean += num;
+        }
+        mean /= size;
+        System.out.printf("The mean is: %.2f%n", mean);
+
+        // Calculate and print standard deviation
+        double sumOfSquares = 0;
+        double sum = 0;
+        for (int num : numbers) {
+            sumOfSquares += Math.pow(num, 2);
+            sum += num;
+        }
+        double variance = sumOfSquares - ((sum * sum)/size);
+        double standardDeviation = Math.sqrt(variance / size);
+        System.out.printf("The standard deviation is: %.2f%n", standardDeviation);
+
+        // Calculate and print # of numbers within one standard deviation of the mean
+        double lowerBound = mean - standardDeviation;
+        double upperBound = mean + standardDeviation;
+        int countWithinOneStdDev = 0;
+        for (int num : numbers) {
+            if (num >= lowerBound && num <= upperBound) {
+                countWithinOneStdDev++;
+            }
+        }
+        System.out.println("The number of values within one standard deviation of the mean is: " + countWithinOneStdDev);
+
     }
 }
