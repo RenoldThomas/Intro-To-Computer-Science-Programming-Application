b039ecb4aa9d138379e05562252f572154be4b0c
Renold Thomas <renoldthomasp@gmail.com>
2025-04-03 10:06:41 -0400

    Create template for Assignment 11

---
+++ renoldthomaslabs/CS0410/Assign11/StatsArray.java
@@ -0,0 +1,12 @@
+// Renold Thomas
+// StatsArray.java
+// 3 April 2025 @ 10-10:50 AM
+// Program to calculate stats based on user input for size and values
+
+package renoldthomaslabs.CS0410.Assign11;
+
+import java.util.Scanner;
+
+public static void main(String[] args) {
+
+}
