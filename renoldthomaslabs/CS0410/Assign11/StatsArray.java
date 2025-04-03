c21107b6e30ec1b3414fe45f5c4af2e99f189419
Renold Thomas <renoldthomasp@gmail.com>
2025-04-03 10:10:52 -0400

    Instantiate size and fill array with user input

--- renoldthomaslabs/CS0410/Assign11/StatsArray.java
+++ renoldthomaslabs/CS0410/Assign11/StatsArray.java
@@ -7,6 +7,17 @@ package renoldthomaslabs.CS0410.Assign11;
 
 import java.util.Scanner;
 
-public static void main(String[] args) {
+public class StatsArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
 
+        int[] numbers = new int[size];
+        System.out.println("Enter " + size + " integers:");
+        for (int i = 0; i < size; i++) {
+            numbers[i] = input.nextInt();
+        }
+        
+    }
 }
